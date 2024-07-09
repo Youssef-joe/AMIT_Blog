55ab84989aed195902df51a8ee9ec9226366ed7d
Youssef-joe <joe92228@gmail.com>
2024-07-09 18:46:05 +0300

    Mern Blog for AMIT Graduation project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Fragment } from 'react'
+
+export default function App() {
+  return (
+    <Fragment>
+      <div className='text-3xl text-red-500'>
+        App
+      </div>
+      
+    </Fragment>
+  )
+}
