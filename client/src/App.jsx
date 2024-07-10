f30be6ae4e27ae510216f71d81484ba4f5214538
Youssef-joe <joe92228@gmail.com>
2024-07-10 18:16:43 +0300

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Fragment } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Projects/Home'
+import About from './Projects/About'
+import Projects from './Projects/Projects'
+import SignIn from './Projects/SignIn'
+import SignUp from './Projects/SignUp'
+import Dashboard from './Projects/Dashboard'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <Fragment>
-      <div className='text-3xl text-red-500'>
-        App
-      </div>
-      
-    </Fragment>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
