b7fba6de1c0d1ce4e1692452d90f0a6bdde53684
Youssef-joe <joe92228@gmail.com>
2024-07-11 10:32:20 +0300

    creating user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const mongoose = require('mongoose')
+require('dotenv').config()
+const PORT = process.env.PORT
 
+const uri = process.env.MONGO_URI
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`)
 })
+
+mongoose
+.connect(uri)
+.then(() => {console.log(`MongoDb is connected`)})
+.catch((er)=> {console.log(er)})
+
